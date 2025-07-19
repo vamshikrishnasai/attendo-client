e0b43b2d24ea2844faf351c7166fb1342a1dc069
vamshikrishnasai <ryagallavamshikrishnasai@gmail.com>
2025-07-19 15:32:41 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import path from "path"
+import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+
+// These 2 lines simulate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +14,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // for local dev if needed
+  }
 })
